efbf9192ae9f3895517ff1be872b9d67976cde03
joceano <alves.joceano@gmail.com>
2021-10-14 17:43:58 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/joceano/hrworker/resources/WorkerResource.java
@@ -0,0 +1,32 @@
+package com.joceano.hrworker.resources;
+
+import com.joceano.hrworker.entities.Worker;
+import com.joceano.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        Worker obj = repository.findById(id).orElseThrow();
+        return ResponseEntity.ok(obj);
+    }
+}
